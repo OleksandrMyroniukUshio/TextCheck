1a70856f4b72afb93b85f452697e863c95b40738
OleksandrMyroniukUshio <mironiuk76@gmail.com>
2022-10-17 22:32:24 +0300

    TextCheck(first commit)

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using sharp28;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Run();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            static void Run()
+            {
+                Console.Clear();
+                Console.WriteLine("(Shouldn't contain >2000 symbols, in english alphabet and no special symbols)" +
+                                   "\nEnter the text: ");
+                string input = ReadText();
+                Validate(input);
+                Console.WriteLine($"Your input has {input.Length} symbols!");
+            }
+            static string ReadText()
+            {
+                string text = Console.ReadLine();
+                if (System.IO.File.Exists(text)) text = File.ReadAllText(text);
+                return text;
+            }
+            static void Validate(string text)
+            {
+                var regexItem = new Regex("^[a-zA-Z0-9 !\"#$%&'()*+,./:;<=>?@\\^_`{|}~-]*$");
+                if (text.Length > 2000) throw new InvalidInputSizeException("Wrong input - text has more than 2000 symbols! \nTry to enter another text!");
+                if (!regexItem.IsMatch(text)) throw new InvalidInputCharactersException("Wrong input - text has non-english alphabet or invalid symbols! \nTry to enter another text!");
+            }
+        }
+    }
+}
---
+++ {CustomException}.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextCheck;
+
+namespace sharp28
+{
+    internal class InvalidInputSizeException : Exception
+    {
+        internal InvalidInputSizeException(string msg) : base(msg)
+        {
+
+        }
+    }
+    internal class InvalidInputCharactersException : Exception
+    {
+        internal InvalidInputCharactersException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
